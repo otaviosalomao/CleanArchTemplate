0b379de57d620d0318e6cfe60b74cfba8b6da1fb
Otávio Martins Salomão <otaviosalomao@gmail.com>
2024-10-16 14:19:14 -0300

    initial structure

---
+++ Application/DepedencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DepedencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = typeof(DepedencyInjection).Assembly;
+            services.AddMediatR(configuration =>
+                configuration.RegisterServicesFromAssembly(assembly));
+            services.AddValidatorsFromAssembly(assembly);
+            return services;
+        }
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,27 @@
+using Application;
+using Infrastructure;
+using Presentation;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+builder.Services.AddPresentation();
+
+builder.Host.UseSerilog((context, configuration) =>
+configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseSerilogRequestLogging();
+app.UseHttpsRedirection();
+
+app.Run();
